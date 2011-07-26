646b4583428ac66bbd9b9107ffc7a9a251dfe925
Timothy Totten <2009@huri.net>
2011-07-26 12:29:06 -0700

    Rewriting how rooms work. More generic, less duplication.

--- nodemoo.js
+++ nodemoo.js
@@ -2,14 +2,18 @@
 
 var net = require('net');
 
-var clients = require('./lib/ClientList.js');
+var world = require('./lib/World.js');
 var clib = require('./lib/Client.js');
 
 var server = net.createServer(function (socket) {
-  var client = clib.newClient(socket, clients);
+  var client = clib.newClient(socket, world);
   client.msg("Welcome to NodeMoo");
-  client.gotoRoom('Start');
-  clients.addClient(client);
+  world.addRoom('start', {
+    'name' : 'The Lobby',
+    'desc' : "Not much to see here yet. It's a big, mostly empty room.",
+  });
+  client.gotoRoom('start');
+  world.addClient(client);
   client.showPrompt();
   socket.on('data', function (data) { 
     client.parse(data); 
