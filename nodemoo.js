c6675c5c70610f85d005b0dc7b94e8026e481a2e
Timothy Totten <2009@huri.net>
2011-07-26 16:07:21 -0700

    Building and linking rooms is now possible, added Mixin support.

--- nodemoo.js
+++ nodemoo.js
@@ -2,16 +2,31 @@
 
 var net = require('net');
 
+// Let's add an "extend" method, with an optional "setup" component.
+Object.defineProperty(Object.prototype, "extend", {
+  enumerable: false,
+  value : function (from) {
+    for (var f in from)
+    { if (f == 'setup')
+        from.setup(this);
+      else
+        this[f] = from[f];
+    }
+  }
+});
+
 var world = require('./lib/World.js');
 var clib = require('./lib/Client.js');
+
+// The starting room. Rather boring.
+world.addRoom('start', {
+  'name' : 'The Lobby',
+  'desc' : "Not much to see here yet. It's a big, mostly empty room.",
+});
 
 var server = net.createServer(function (socket) {
   var client = clib.newClient(socket, world);
   client.msg("Welcome to NodeMoo");
-  world.addRoom('start', {
-    'name' : 'The Lobby',
-    'desc' : "Not much to see here yet. It's a big, mostly empty room.",
-  });
   client.gotoRoom('start');
   world.addClient(client);
   client.showPrompt();
