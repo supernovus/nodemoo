6faaebd19c731edf1c00b55009fd19a4b33523d3
Timothy Totten <2009@huri.net>
2011-07-27 11:07:50 -0700

    Implemented a config file, first steps towards persistence. Refactored slightly.

--- nodemoo.js
+++ nodemoo.js
@@ -2,33 +2,13 @@
 
 var net = require('net');
 
-// Let's add an "extend" method, with an optional "setup" component.
-Object.defineProperty(Object.prototype, "extend", {
-  enumerable: false,
-  value : function (from) {
-    for (var f in from)
-    { if (f == 'setup')
-        from.setup(this);
-      else
-        this[f] = from[f];
-    }
-  }
-});
-
 var world = require('./lib/World.js');
-var clib = require('./lib/Client.js');
-
-// The starting room. Rather boring.
-world.addRoom('start', {
-  'name' : 'The Lobby',
-  'desc' : "Not much to see here yet. It's a big, mostly empty room.",
-});
+var config = world.load('./game.json');
 
 var server = net.createServer(function (socket) {
-  var client = clib.newClient(socket, world);
+  var client = world.addClient(socket);
   client.msg("Welcome to NodeMoo");
   client.gotoRoom('start');
-  world.addClient(client);
   client.showPrompt();
   socket.on('data', function (data) { 
     client.parse(data); 
